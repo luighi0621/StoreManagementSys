4c15d4df1938a38cd9da72e7b44a1d476e22ec10
Luis Choque Castro <luis.choque@avantica.net>
2018-06-27 10:33:12 -0400

    Unit test for facade pattern

---
+++ StoreManagement.Tests/DesignPatterns/FacadePatternTests.cs
@@ -0,0 +1,48 @@
+﻿using FacadePattern;
+using FacadePattern.SubSystems;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManagement.Tests.DesignPatterns
+{
+    [TestFixture]
+    public class FacadePatternTests
+    {
+        private Mock<IBank> _mockBank;
+        private Mock<ICredit> _mockCredit;
+        private Mock<ILoan> _mockLoan;
+        private BNB _BNB;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockBank = new Mock<IBank>();
+            _mockBank.Setup(b => b.HasSufficientSavings(It.IsAny<Customer>(), It.IsAny<int>())).Returns(true);
+
+            _mockLoan = new Mock<ILoan>();
+            _mockLoan.Setup(l => l.HasNoBadLoans(It.IsAny<Customer>())).Returns(true);
+
+            _mockCredit = new Mock<ICredit>();
+            _mockCredit.Setup(c => c.HasGoodCredit(It.IsAny<Customer>())).Returns(true);
+
+            _BNB = new BNB(_mockBank.Object, _mockLoan.Object, _mockCredit.Object);
+        }
+
+        [Test, Category("Facade")]
+        public void CustomerAskForCredit()
+        {
+            Customer c = new Customer("Luis");
+            var response = _BNB.IsElegible(c, 5000);
+            _mockBank.Verify(b=> b.HasSufficientSavings(It.IsAny<Customer>(), It.IsAny<int>()));
+            _mockLoan.Verify(l => l.HasNoBadLoans(It.IsAny<Customer>()));
+            _mockCredit.Verify(cr=> cr.HasGoodCredit(It.IsAny<Customer>()));
+            Assert.IsTrue(response);
+        }
+
+    }
+}
