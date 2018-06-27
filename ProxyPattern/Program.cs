5b51dad285b7b50083132b9cef757db7e5e1c12a
Luis Choque Castro <luis.choque@avantica.net>
2018-06-27 10:30:13 -0400

    proxy pattern project added

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using ProxyPattern.Subjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathProxy proxy = new MathProxy();
+
+            Console.WriteLine("4 + 2 = {0}", proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = {0}", proxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = {0}", proxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = {0}", proxy.Div(4, 2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProxyPattern/Subjects/MathProxy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern.Subjects
+{
+    public class MathProxy : IMath
+    {
+        public MathProxy(IMath ma)
+        {
+            _math = ma;
+        }
+        public MathProxy()
+        {
+            _math = new Math();
+        }
+        private IMath _math;
+        public double Add(double x, double y)
+        {
+            return _math.Add(x, y);
+        }
+
+        public double Div(double x, double y)
+        {
+            return _math.Div(x, y);
+        }
+
+        public double Mul(double x, double y)
+        {
+            return _math.Mul(x, y);
+        }
+
+        public double Sub(double x, double y)
+        {
+            return _math.Sub(x, y);
+        }
+    }
+}
