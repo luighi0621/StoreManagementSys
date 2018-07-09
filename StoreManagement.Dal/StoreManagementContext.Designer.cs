0b1e948211a4de578cc402ea91026a0ab97dce26
Luis Choque Castro <luis.choque@avantica.net>
2018-07-09 09:53:56 -0400

    user web api implemented

--- StoreManagement.Dal/StoreManagementContext.Designer.cs
+++ StoreManagement.Dal/StoreManagementContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\luis.choque\Projects\StoreManagement\StoreManagement.Dal\StoreManagementContext.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\luis.choque\Projects\StoreManagement\StoreManagement.Dal\StoreManagementContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ StoreManagement/Controllers/api/UsersController.cs
@@ -0,0 +1,81 @@
+﻿using StoreManagement.Dal.Interfaces;
+using StoreManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+
+namespace StoreManagement.Controllers.api
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class UsersController : ApiController
+    {
+        private IUserRepository _userRepository;
+        public UsersController(IUserRepository repo)
+        {
+            _userRepository = repo;
+        }
+        // GET api/<controller>
+        [ResponseType(typeof(IEnumerable<User>))]
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, _userRepository.GetAll());
+        }
+
+        // GET api/<controller>/5
+        [ResponseType(typeof(User))]
+        public HttpResponseMessage Get(int id)
+        {
+            User toFind = _userRepository.Get(c => c.ID == id);
+            if (toFind == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "User not found");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, toFind);
+        }
+
+        // POST api/<controller>
+        [ResponseType(typeof(User))]
+        public HttpResponseMessage Post([FromBody]User user)
+        {
+            if (user != null)
+            {
+                _userRepository.Create(user);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, user);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]User user)
+        {
+            User ToUpdate = _userRepository.Get(c => c.ID == id);
+            if (ToUpdate == null)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotModified, "User to update does not exits."));
+            }
+
+            ToUpdate.ID = user.ID;
+            ToUpdate.Firstname = user.Firstname;
+            ToUpdate.Lastname = user.Lastname;
+            ToUpdate.Login = user.Login;
+            ToUpdate.Email = user.Email;
+            ToUpdate.Phone = user.Phone;
+            _userRepository.Update(ToUpdate);
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            User ToDelete = _userRepository.Get(c => c.ID == id);
+            if (ToDelete == null)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "User to delete does not exits."));
+            }
+            _userRepository.Delete(ToDelete);
+        }
+    }
+}
